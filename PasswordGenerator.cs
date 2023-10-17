d4fffacc8a5329a0d894750390e6c4d4f5275161
Nusrat Siddique <113282774+ns-tuli@users.noreply.github.com>
2023-10-18 00:28:57 +0600

    Update PasswordGenerator.cs added ReadRequiredSymbol funtion
    read required symbol function

--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -78,5 +78,20 @@ namespace CipherShield_Beta0._2
                 Console.WriteLine("\nInvalid input. Please enter 'y' for yes or 'n' for no.");
             }
         }
+
+
+          private static char ReadRequiredSymbol()
+        {
+            char symbol;
+            while (true)
+            {
+                Console.Write("Enter the required symbol: ");
+                if (char.TryParse(Console.ReadLine(), out symbol))
+                {
+                    return symbol;
+                }
+                Console.WriteLine("Invalid input. Please enter a single character.");
+            }
+        }
     }
 }
