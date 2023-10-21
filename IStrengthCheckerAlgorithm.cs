9a1abbdd4a2204b2de77ef033e1062c634e6a358
Saadman Sakib <134169023+saadmansakib47@users.noreply.github.com>
2023-10-21 23:19:16 +0600

    Add files via upload
    In this change, new additions are modification of "StrengthChecker" class and 4 necessary metrics to assist it ("ShannonEntropy" for randomness checking, "KeyboardPatternDetect" to detect common sequential patterns, "RepetitiveCharacterDetect" to identify more than two consecutive characters and "LengthDetect" to check the length). Masking has been applied, and relevant information can be visible in the console after user presses a certain key.

---
+++ IStrengthCheckerAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿namespace CipherShield_Beta
+{
+    public interface IStrengthCheckerAlgorithms
+    {
+        // Method to calculate the strength of a password based on the algorithm.
+        int CheckStrength(string password);
+
+        // Method to provide feedback on the password's strength.
+        string GetFeedback(int strength);
+    }
+}
---
+++ KeyBoardPatternDetect.cs
@@ -0,0 +1,72 @@
+﻿using CipherShield_Beta;
+public class KeyboardPatternDetect : IStrengthCheckerAlgorithms
+{
+    public int CheckStrength(string password)
+    {
+        if (ContainsRepeatingPatterns(password) || ContainsCommonKeyboardPatterns(password))
+        {
+            return 0; // Password contains keyboard patterns, considered weak.
+        }
+
+        return 90; // Password does not contain keyboard patterns, considered strong.
+    }
+
+    public string GetFeedback(int strength)
+    {
+        return strength >= 80 ? "Low" : "High";
+    }
+
+
+    private bool ContainsCommonKeyboardPatterns(string password)
+    {
+        List<string> patterns = new List<string>
+    {
+        "abcdefghijklmnopqrstuvwxyz",
+        "qwertyuiop",
+        "asdfghjkl",
+        "zxcvbnm",
+        "1234567890",
+        "password"
+    };
+
+        foreach (string pattern in patterns)
+        {
+            for (int i = 0; i <= pattern.Length - 4; i++)
+            {
+                string subPattern = pattern.Substring(i, 4);
+                if (password.Contains(subPattern) || password.Contains(ReverseString(subPattern)))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+
+
+    private bool ContainsRepeatingPatterns(string password)
+    {
+        // Check for repeating patterns of length 3 or more.
+        for (int i = 0; i < password.Length - 2; i++)
+        {
+            string subsequence = password.Substring(i, 3);
+            if (password.LastIndexOf(subsequence) > i)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
+
