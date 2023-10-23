9f5512c90f03ba443701a4badda392ef8ffd5f48
Nazifa Tasneem <112499963+nazifatasneem13@users.noreply.github.com>
2023-10-24 00:10:49 +0600

    unit testing for shannon entropy and length detect

---
+++ LengthDetectTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CipherShield;
+
+namespace CypherShield_TestProject
+{
+    [TestClass]
+    public class LengthDetectTest
+    {
+        [TestMethod]
+        public void CheckStrength_LongPassword_Returns100()
+        {
+            // Arrange
+            LengthDetect lengthDetect = new LengthDetect();
+            string longPassword = "ThisIsAVeryLongPassword";
+
+            // Act
+            int strength = lengthDetect.CheckStrength(longPassword);
+
+            // Assert
+            Assert.AreEqual(100, strength);
+        }
+
+        [TestMethod]
+        public void CheckStrength_ModerateLengthPassword_Returns70()
+        {
+            // Arrange
+            LengthDetect lengthDetect = new LengthDetect();
+            string moderatePassword = "MedPwd12";
+
+            // Act
+            int strength = lengthDetect.CheckStrength(moderatePassword);
+
+            // Assert
+            Assert.AreEqual(70, strength);
+        }
+
+        [TestMethod]
+        public void CheckStrength_ShortPassword_Returns40()
+        {
+            // Arrange
+            LengthDetect lengthDetect = new LengthDetect();
+            string shortPassword = "Pwd123";
+
+            // Act
+            int strength = lengthDetect.CheckStrength(shortPassword);
+
+            // Assert
+            Assert.AreEqual(40, strength);
+        }
+
+        [TestMethod]
+        public void CheckStrength_EmptyPassword_Returns40()
+        {
+            // Arrange
+            LengthDetect lengthDetect = new LengthDetect();
+            string emptyPassword = string.Empty;
+
+            // Act
+            int strength = lengthDetect.CheckStrength(emptyPassword);
+
+            // Assert
+            Assert.AreEqual(40, strength);
+        }
+
+        /*
+        [TestMethod]
+        public void CheckStrength_NullPassword_Returns40()
+        {
+            // Arrange
+            LengthDetect lengthDetect = new LengthDetect();
+            string nullPassword = null;
+
+            // Act
+            int strength = lengthDetect.CheckStrength(nullPassword);
+
+            // Assert
+            Assert.AreEqual(40, strength);
+        }
+        */
+    }
+}
---
+++ ShannonEntropyTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CipherShield;
+
+namespace CypherShield_TestProject
+{
+    [TestClass]
+    public class ShannonEntropyTest
+    {
+        /*
+        [TestMethod]
+        public void CheckStrength_WhenStrongPassword_Returns100()
+        {
+            // Arrange
+            ShannonEntropy shannonEntropy = new ShannonEntropy();
+            string strongPassword = "P@ssw0rd!";
+
+            // Act
+            int strength = shannonEntropy.CheckStrength(strongPassword);
+
+            // Assert
+            Assert.AreEqual(100, strength);
+        }
+        */
+
+        [TestMethod]
+        public void CheckStrength_WhenWeakPassword_ReturnsLessThan100()
+        {
+            // Arrange
+            ShannonEntropy shannonEntropy = new ShannonEntropy();
+            string weakPassword = "password123";
+
+            // Act
+            int strength = shannonEntropy.CheckStrength(weakPassword);
+
+            // Assert
+            Assert.IsTrue(strength < 100);
+        }
+
+        [TestMethod]
+        public void CheckStrength_WhenEmptyPassword_Returns0()
+        {
+            // Arrange
+            ShannonEntropy shannonEntropy = new ShannonEntropy();
+            string emptyPassword = string.Empty;
+
+            // Act
+            int strength = shannonEntropy.CheckStrength(emptyPassword);
+
+            // Assert
+            Assert.AreEqual(0, strength);
+        }
+
+        /*
+        [TestMethod]
+        public void CheckStrength_WhenNullPassword_Returns0()
+        {
+            // Arrange
+            ShannonEntropy shannonEntropy = new ShannonEntropy();
+            string nullPassword = null;
+
+            // Act
+            int strength = shannonEntropy.CheckStrength(nullPassword);
+
+            // Assert
+            Assert.AreEqual(0, strength);
+        }
+        */
+    }
+}
